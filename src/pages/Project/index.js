7aa4e3108bda90714bced58bb0160727d90de335
lincoln <lincoln-md@hotmail.com>
2021-12-05 15:52:52 -0300

    finalizando cadastro de horas

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { Context } from '../../context/ProjectContext';
+import Swal from "sweetalert2";
+import api from "../../services/api";
 
 import plus from '../../assets/plusWhite.svg';
 
@@ -12,58 +14,126 @@ import { CardHour } from "../../components/Card"
 
 export default function Project(props) {
 
-  const { 
-    user, 
-    loading, 
-    GetProfile, 
-    
-    GetHours, 
+  const {
+    user,
+    loading,
+    GetProfile,
+
+    GetHours,
     hours } = useContext(Context);
 
-  const [project, setProject] = useState({})
+  const [project, setProject] = useState({});
+  const [hoursWorked, setHoursWorked] = useState('');
+  const [day, setDay] = useState('');
+
+  function handleHours(event){
+    setHoursWorked(event.target.value);
+  }
+
+  function handleDay(event){
+    setDay(event.target.value);
+  }
+
+  function formatStringData(data) {
+    let dia = data.split("-")[2];
+    let mes = data.split("-")[1];
+    let ano = data.split("-")[0];
+
+    return dia + '/' + mes + '/' + ano
+  }
+
+  async function PostHour() {
+
+    try {
+      const token = localStorage.getItem('token');
+
+      const hoursConvert = Number(hoursWorked)
+
+      await api.post('/hours', 
+      {
+        "hours": hoursConvert,
+        "day": day,
+        "project": project?._id,
+        "user": user?._id
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${JSON.parse(token)}`
+        }
+      })
+
+      setHoursWorked('');
+      setDay('');
+
+      Swal.fire({
+        title: 'Horas incluídas',
+        confirmButtonText: 'ok',
+        icon: 'success',
+      })
+
+      GetHours();
+    } catch (err) {
+      console.log(err)
+     
+      setHoursWorked('');
+      setDay('');
+
+      Swal.fire({
+        title: 'Ops, não foi possível cadastrar a carga horária',
+        confirmButtonText: 'ok',
+        icon: 'error',
+      })
+    }
+  }
+
+  const filteredHours = hours.filter((item) => (
+    item?.project === project?._id
+  ));
 
   useEffect(() => {
     GetProfile();
     GetHours();
     setProject(props.location.state.project);
-  }, [])
-
-  const filteredHours = hours.filter((item) => (
-    item?.project === project?._id
-  ))
-
-  function formatStringData(data) {
-    var dia = data.split("-")[2];
-    var mes = data.split("-")[1];
-    var ano = data.split("-")[0];
-
-    return dia + '/' + mes + '/' + ano 
-  }
+  }, []);
 
   return (
     <>
       <Loader isLoading={loading} />
+
       <ContainerMain>
+
         <Aside user={user} />
         <section className="container-project">
+
           <ContainerSecondary>
             <h3>Cadastrar Carga horária</h3>
             <div className="content-container-project">
-              <InputSmall
-                placeholder="Horas trabalhadas"
-                type="text"
-                noMargin />
-              <InputSmall
-                placeholder="Data"
-                type="date"
-                noMargin />
+
+              <div className="content-inputs">
+                <InputSmall
+                  placeholder="Horas trabalhadas"
+                  type="number"
+                  noMargin
+                  onChange={handleHours}
+                  value={hoursWorked}/>
+                <InputSmall
+                  placeholder="Data"
+                  type="date"
+                  noMargin
+                  onChange={handleDay}
+                  value={day}/>
+              </div>
+
               <Button
                 type="button"
-                small>
+                small
+                onClick={PostHour}>
                 <img src={plus} alt={"cadastro"} />&nbsp; Cadastrar
               </Button>
+
             </div>
           </ContainerSecondary>
+
           <Container>
             <div className="container-data">
               <h3>{project.name}</h3>
@@ -76,15 +146,13 @@ export default function Project(props) {
               </div>
             </div>
             <div className="container-cards">
-              {filteredHours?.map((item) => 
-              <CardHour key={item?._id}>
-                <strong>{formatStringData(item?.day)}{" - "}&nbsp;</strong>
-                <span>{item?.hours + " Horas"}</span>
-              </CardHour>
+              {filteredHours?.map((item) =>
+                <CardHour key={item?._id}>
+                  <strong>{formatStringData(item?.day)}{" - "}&nbsp;</strong>
+                  <span>{item?.hours + " Horas"}</span>
+                </CardHour>
               )}
-
             </div>
-
           </Container>
         </section>
       </ContainerMain>
